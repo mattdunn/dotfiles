4b23c876760e343ac241842d9fde1c705ec1893b
Matt Dunn <contact@mattonsoftware.com>
2016-01-12 15:28:44 +1100

    added command-t vim plugin

--- install.sh
+++ install.sh
@@ -30,7 +30,18 @@ install_vim_plugins(){
   mkdir -p $HOME/.vim/autoload $HOME/.vim/bundle
   cp -r vim/package/tpope-vim-pathogen/autoload/ $HOME/.vim/autoload
   cp -r vim/package/nerdtree/ $HOME/.vim/bundle/nerdtree
+  cp -r vim/package/command-t/ $HOME/.vim/bundle/command-t
   cp -r vim/colors/ $HOME/.vim/bundle
+
+  # compile c extension for command-t vim package
+  # nb: the version of ruby invoked here needs to be the same
+  #     as the version that vim was installed/compiled against
+  #     i.e. the system version
+  pushd .
+  cd $HOME/.vim/bundle/command-t/ruby/command-t
+  ruby extconf.rb
+  make
+  popd
 }
 
 symlink_dotfiles
